4fdd9dd49db6a3dc7938c3a0443d14db8649de4f
Ivan Gandacov <ivan.gandacov@gmail.com>
2023-02-08 14:58:29 +0100

    Add user events tracking

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+declare let gtag:Function;
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  onGooglePlayClick() {
+    gtag('event', 'google_play_click', {
+      event_category: 'button_click',
+      event_label: 'Get it on Google Play Click',
+      value: 'google_play',
+    });
+  }
 
+  onAppleAppStoreClick() {
+    gtag('event', 'apple_app_store_click', {
+      event_category: 'button_click',
+      event_label: 'Download on the App Store Click',
+      value: 'apple_app_store',
+    });
+  }
 }
